44d406f8f15c50b600e6d19a85c21dbdecf688ad
Jan Woźniak <jnwznk@gmail.com>
2022-12-05 22:57:12 +0100

    First part done

--- day_1/src/lib.rs
+++ day_1/src/lib.rs
@@ -1,9 +1,19 @@
+use std::collections::LinkedList;
 use std::fs;
 
-fn load_data(path: &str) {
+pub fn get_highest_calories(path: &str) -> u32 {
     let data = fs::read_to_string(path).expect("Unable to read file");
+    let mut calories = LinkedList::<u32>::new();
+    calories.push_front(0);
+    data.split('\n').for_each(|s| {
+        if s.is_empty() {
+            calories.push_back(0);
+        } else {
+            *calories.back_mut().unwrap() += s.trim().parse().unwrap_or(0);
+        }
+    });
 
-    println!("{}", data);
+    calories.into_iter().max().unwrap_or(0)
 }
 
 #[cfg(test)]
@@ -12,6 +22,13 @@ mod tests {
 
     #[test]
     fn load_test_data() {
-        load_data("test_data.txt");
+        let calories = get_highest_calories("test_data.txt");
+        assert_eq!(calories, 24000);
+    }
+
+    #[test]
+    fn assigned_values() {
+        let calories = get_highest_calories("input.txt");
+        println!("{}", calories);
     }
 }
