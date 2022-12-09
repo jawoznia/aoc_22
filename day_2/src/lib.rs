13423ba9197d2a9ef584995f4491d15539e25b85
Jan Woźniak <jnwznk@gmail.com>
2022-12-09 17:44:50 +0100

    day_2 first part

--- day_2/src/lib.rs
+++ day_2/src/lib.rs
@@ -1,14 +1,62 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use anyhow::Result;
+
+pub fn load_data(file: &str) -> Result<u32> {
+    let file = File::open(file)?;
+    let reader = BufReader::new(file);
+    let mut score = 0;
+    reader
+        .lines()
+        .filter_map(|l| l.ok())
+        .for_each(|l| score += round_score(&l));
+    Ok(score)
+}
+
+fn round_score(line: &str) -> u32 {
+    if line.starts_with('A') {
+        if line.ends_with('X') {
+            return 4;
+        } else if line.ends_with('Y') {
+            return 8;
+        } else {
+            return 3;
+        }
+    } else if line.starts_with('B') {
+        if line.ends_with('X') {
+            return 1;
+        } else if line.ends_with('Y') {
+            return 5;
+        } else {
+            return 9;
+        }
+    } else {
+        if line.ends_with('X') {
+            return 7;
+        } else if line.ends_with('Y') {
+            return 2;
+        } else {
+            return 6;
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::load_data;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_data() {
+        let score = load_data("test_data.txt").unwrap();
+        assert_eq!(score, 15);
+    }
+
+    #[test]
+    fn test_first_input() {
+        let score = load_data("input.txt").unwrap();
+        assert_eq!(score, 10718)
     }
 }
