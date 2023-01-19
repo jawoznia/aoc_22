36aee3f38581c1c5f5493a963e7b43fb160c3dab
Jan Woźniak <jnwznk@gmail.com>
2023-01-19 22:12:58 +0100

    Vec instead of String to allow sort

--- day_3/src/lib.rs
+++ day_3/src/lib.rs
@@ -5,13 +5,13 @@ use std::io::{BufRead, BufReader};
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct Rucksack {
-    left: String,
-    right: String,
+    left: Vec<char>,
+    right: Vec<char>,
 }
 type Rucksacks = Vec<Rucksack>;
 
-impl From<(String, String)> for Rucksack {
-    fn from(c: (String, String)) -> Self {
+impl From<(Vec<char>, Vec<char>)> for Rucksack {
+    fn from(c: (Vec<char>, Vec<char>)) -> Self {
         Rucksack {
             left: c.0,
             right: c.1,
@@ -27,8 +27,25 @@ pub fn load_data(file: &str) -> Result<Rucksacks> {
         .filter_map(|l| l.ok())
         .map(|mut l| {
             let r = l.split_off(l.len() / 2);
+
+            let mut l: Vec<_> = l.chars().collect();
+            let mut r: Vec<_> = r.chars().collect();
+            r.sort();
+            l.sort();
+
             (l, r).into()
         })
         .collect();
     Ok(rucksacks)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_data() {
+        let rucksacks = load_data("example.txt").unwrap();
+        println!("RS => {:#?}", rucksacks);
+    }
 }
