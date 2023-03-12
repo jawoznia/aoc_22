5c39fb324e57d704e4137ad3eddfb7aea0dbc30b
Jan Woźniak <jnwznk@gmail.com>
2023-03-13 00:04:54 +0100

    day_4: data loaded

---
+++ day_4/src/lib.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+#[derive(Debug)]
+pub struct SectionPair {
+    pub left: (u32, u32),
+    pub right: (u32, u32),
+}
+
+pub fn load_data(file: &str) -> Result<Vec<SectionPair>> {
+    let file = File::open(file)?;
+    let reader = BufReader::new(file);
+    let pair: Vec<_> = reader
+        .lines()
+        .filter_map(|l| l.ok())
+        .map(|l| -> Result<_> {
+            let borders: Vec<_> = l
+                .split([',', '-'])
+                .map(|s| s.trim().parse::<u32>())
+                .filter_map(|s| s.ok())
+                .collect();
+            assert_eq!(borders.len(), 4);
+
+            Ok(SectionPair {
+                left: (borders[0], borders[1]),
+                right: (borders[2], borders[3]),
+            })
+        })
+        .filter_map(|l| l.ok())
+        .collect();
+
+    Ok(pair)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let _sections = load_data("example.txt").unwrap();
+    }
+}
