8dd18aa82f90dfbdad2d2cdf679b379994eda92d
Jan Woźniak <jnwznk@gmail.com>
2023-03-15 00:06:12 +0100

    Renamed MarkerFinder to Detector

--- day_6/src/lib.rs
+++ day_6/src/lib.rs
@@ -5,41 +5,41 @@ use std::io::{BufRead, BufReader};
 use anyhow::Result;
 
 #[derive(Debug)]
-pub struct MarkerFinder(String);
+pub struct Detector(String);
 
 #[derive(Debug)]
-pub struct MarkerFinders(Vec<MarkerFinder>);
+pub struct Detectors(Vec<Detector>);
 
-impl FromIterator<MarkerFinder> for MarkerFinders {
-    fn from_iter<T: IntoIterator<Item = MarkerFinder>>(iter: T) -> Self {
+impl FromIterator<Detector> for Detectors {
+    fn from_iter<T: IntoIterator<Item = Detector>>(iter: T) -> Self {
         Self(iter.into_iter().collect())
     }
 }
 
-impl MarkerFinders {
+impl Detectors {
     pub fn new(file: &str) -> Result<Self> {
         let file = File::open(file)?;
         let reader = BufReader::new(file);
-        let finders: Self = reader
+        let detectors: Self = reader
             .lines()
             .filter_map(|l| l.ok())
-            .map(|l| MarkerFinder(l))
+            .map(|l| Detector(l))
             .collect();
 
-        Ok(finders)
+        Ok(detectors)
     }
 
-    pub fn find(&self) -> Vec<usize> {
+    pub fn find_markers(&self) -> Vec<usize> {
         self.0
             .iter()
-            .map(|mf| mf.find())
+            .map(|mf| mf.find_marker())
             .filter_map(|marker| marker)
             .collect()
     }
 }
 
-impl MarkerFinder {
-    pub fn find(&self) -> Option<usize> {
+impl Detector {
+    pub fn find_marker(&self) -> Option<usize> {
         for top in 4..self.0.len() {
             let bottom = top - 4;
 
@@ -56,16 +56,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example() {
-        let marker_finders = MarkerFinders::new("example.txt").unwrap();
-        let markers = marker_finders.find();
-        assert_eq!(markers, vec![5, 6, 10, 11]);
+    fn example_marker() {
+        let detectors = Detectors::new("example.txt").unwrap();
+        let markers = detectors.find_markers();
+        assert_eq!(markers, vec![7, 5, 6, 10, 11]);
     }
 
     #[test]
-    fn input() {
-        let marker_finders = MarkerFinders::new("input.txt").unwrap();
-        let markers = marker_finders.find();
+    fn input_marker() {
+        let detectors = Detectors::new("input.txt").unwrap();
+        let markers = detectors.find_markers();
         assert_eq!(markers, vec![1655]);
     }
 }
