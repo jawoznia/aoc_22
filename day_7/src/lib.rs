49f292f3d90b2e99f9892b5e3ed1664ab6e3d10a
Jan Woźniak <jnwznk@gmail.com>
2023-03-24 00:19:03 +0100

    Data structure and load logic partially created

--- day_7/src/lib.rs
+++ day_7/src/lib.rs
@@ -1,23 +1,62 @@
-pub File {
-    pub name: String,
-    pub size: u64,
-}
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 
-pub struct Directory {
-    pub name: String,
-    pub files: Vec<File>,
-    pub sub_directories: Vec<Directory>,
-}
+use anyhow::Result;
 
+#[derive(Debug, Default)]
+pub struct Directories(HashMap<PathBuf, u64>);
+
+#[derive(Debug)]
 pub struct FileSystem {
-    pub root: Directory,
-    pub current_dir:Directory 
+    pub current_dir: PathBuf,
+    pub directories: Directories,
+}
+
+impl FileSystem {
+    pub fn new(file: &str) -> Result<FileSystem> {
+        let file = std::fs::File::open(file)?;
+        let reader = BufReader::new(file);
+
+        let mut file_system = FileSystem {
+            current_dir: PathBuf::new(),
+            directories: Directories::default(),
+        };
+        reader
+            .lines()
+            .flatten()
+            .filter(|line| !line.starts_with("$ ls") && !line.starts_with("dir"))
+            .for_each(|line| {
+                let words: Vec<&str> = line.split_whitespace().collect();
+
+                if words[0] == "$" {
+                    let new_path = file_system.current_dir.join(words[2]);
+                    file_system.directories.0.insert(new_path.clone(), 0);
+                    file_system.current_dir = new_path;
+                } else {
+                    match file_system.directories.0.get_mut(&file_system.current_dir) {
+                        Some(size) => {
+                            size.checked_add(words[0].parse::<u64>().unwrap())
+                                .unwrap_or_else(|| {
+                                    panic!("Overflow: {:#?}", file_system.current_dir)
+                                });
+                        }
+
+                        None => panic!("No such directory: {:#?}", file_system.current_dir),
+                    }
+                }
+            });
+
+        Ok(file_system)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let fs = FileSystem::new("example.txt").unwrap();
+        println!("{:#?}", fs);
     }
 }
