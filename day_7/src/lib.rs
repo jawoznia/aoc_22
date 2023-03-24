5cd2b0a4404f60719ff41da01c3542acf2f51f95
Jan Woźniak <jnwznk@gmail.com>
2023-03-24 01:08:15 +0100

    Removed FileSystem struct

--- day_7/src/lib.rs
+++ day_7/src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 
@@ -7,21 +8,19 @@ use anyhow::Result;
 #[derive(Debug, Default)]
 pub struct Directories(HashMap<PathBuf, u64>);
 
-#[derive(Debug)]
-pub struct FileSystem {
-    pub current_dir: PathBuf,
-    pub directories: Directories,
+impl From<HashMap<PathBuf, u64>> for Directories {
+    fn from(c: HashMap<PathBuf, u64>) -> Self {
+        Self(c)
+    }
 }
 
-impl FileSystem {
-    pub fn new(file: &str) -> Result<FileSystem> {
-        let file = std::fs::File::open(file)?;
+impl Directories {
+    pub fn new(file: &str) -> Result<Directories> {
+        let file = File::open(file)?;
         let reader = BufReader::new(file);
 
-        let mut file_system = FileSystem {
-            current_dir: PathBuf::new(),
-            directories: Directories::default(),
-        };
+        let mut directories = HashMap::new();
+        let mut current_dir = PathBuf::new();
         reader
             .lines()
             .flatten()
@@ -31,26 +30,26 @@ impl FileSystem {
 
                 if words[0] == "$" {
                     if words[2] == ".." {
-                        file_system.current_dir.pop();
-                    } else if words[2] == "/" && !file_system.directories.0.is_empty() {
-                        file_system.current_dir = PathBuf::new();
-                        file_system.current_dir.push("/");
+                        current_dir.pop();
+                    } else if words[2] == "/" && !directories.is_empty() {
+                        current_dir = PathBuf::new();
+                        current_dir.push("/");
                     } else {
-                        let new_path = file_system.current_dir.join(words[2]);
-                        file_system.directories.0.insert(new_path.clone(), 0);
-                        file_system.current_dir = new_path;
+                        let new_path = current_dir.join(words[2]);
+                        directories.insert(new_path.clone(), 0);
+                        current_dir = new_path;
                     }
                 } else {
-                    match file_system.directories.0.get_mut(&file_system.current_dir) {
+                    match directories.get_mut(&current_dir) {
                         Some(size) => {
                             *size += words[0].parse::<u64>().unwrap();
                         }
-                        None => panic!("No such directory: {:#?}", file_system.current_dir),
+                        None => panic!("No such directory: {:#?}", current_dir),
                     }
                 }
             });
 
-        Ok(file_system)
+        Ok(directories.into())
     }
 }
 
@@ -59,7 +58,7 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let fs = FileSystem::new("example.txt").unwrap();
+        let fs = Directories::new("example.txt").unwrap();
         println!("{:#?}", fs);
     }
 }
