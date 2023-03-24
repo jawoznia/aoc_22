8bcba87ac3c4a80002539e05fe494794e12d6c6c
Jan Woźniak <jnwznk@gmail.com>
2023-03-24 19:55:19 +0100

    Input data loaded

--- day_8/src/lib.rs
+++ day_8/src/lib.rs
@@ -1,5 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use anyhow::Result;
+
+#[derive(Debug)]
+pub struct Matrix {
+    pub data: Vec<Vec<u8>>,
+}
+
+impl Matrix {
+    pub fn new(file: &str) -> Result<Self> {
+        let data = std::fs::read_to_string(file)?
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|s| s.to_digit(10).unwrap() as u8)
+                    .collect()
+            })
+            .collect();
+        Ok(Self { data })
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +24,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn example() {
+        let matrix = Matrix::new("example.txt").unwrap();
+        println!("{:#?}", matrix.data)
     }
 }
