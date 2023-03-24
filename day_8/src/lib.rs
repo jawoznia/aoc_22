584ef77b6870261b9771a38e9c5f27476acd14eb
Jan Woźniak <jnwznk@gmail.com>
2023-03-24 20:28:42 +0100

    Neighbour trees check. Need to check all the way to the border now

--- day_8/src/lib.rs
+++ day_8/src/lib.rs
@@ -1,9 +1,7 @@
 use anyhow::Result;
 
 #[derive(Debug)]
-pub struct Matrix {
-    pub data: Vec<Vec<u8>>,
-}
+pub struct Matrix(Vec<Vec<u8>>);
 
 impl Matrix {
     pub fn new(file: &str) -> Result<Self> {
@@ -15,7 +13,38 @@ impl Matrix {
                     .collect()
             })
             .collect();
-        Ok(Self { data })
+        Ok(Self(data))
+    }
+
+    // Border trees are always visible
+    pub fn count_visible_trees(&self) -> usize {
+        let matrix = &self.0;
+        let mut count = matrix.len() * 2 + matrix[0].len() * 2 - 4;
+        println!("Border trees: {}", count);
+        for c in 1..matrix.len() - 1 {
+            for r in 1..matrix[c].len() - 1 {
+                print!("{}", matrix[c][r]);
+                if matrix[c][r] > matrix[c - 1][r]
+                    || matrix[c][r] > matrix[c + 1][r]
+                    || matrix[c][r] > matrix[c][r - 1]
+                    || matrix[c][r] > matrix[c][r + 1]
+                {
+                    print!("+");
+                    count += 1;
+                }
+            }
+            println!("");
+        }
+        count
+    }
+
+    pub fn print(&self) {
+        for row in &self.0 {
+            for col in row {
+                print!("{}", col);
+            }
+            println!();
+        }
     }
 }
 
@@ -26,6 +55,7 @@ mod tests {
     #[test]
     fn example() {
         let matrix = Matrix::new("example.txt").unwrap();
-        println!("{:#?}", matrix.data)
+        matrix.print();
+        assert_eq!(matrix.count_visible_trees(), 21);
     }
 }
