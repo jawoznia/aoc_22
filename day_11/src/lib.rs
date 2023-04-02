917097e597d8f1f077ab45a3746eb90f1c697cdf
Jan Woźniak <jnwznk@gmail.com>
2023-04-02 23:48:27 +0200

    Split instantiation between structs

--- day_11/src/lib.rs
+++ day_11/src/lib.rs
@@ -38,6 +38,34 @@ pub struct Test {
     pub unmatched_receiver: u32,
 }
 
+impl Test {
+    pub fn new(divisor: &str, matched: &str, unmatched: &str) -> Result<Self> {
+        let divisor = divisor
+            .split_whitespace()
+            .nth(3)
+            .ok_or_else(|| anyhow::anyhow!("Wrong divisor format"))?
+            .parse::<u32>()?;
+
+        let matched_receiver = matched
+            .split_whitespace()
+            .nth(5)
+            .ok_or_else(|| anyhow::anyhow!("Wrong matched monkey identifier"))?
+            .parse::<u32>()?;
+
+        let unmatched_receiver = unmatched
+            .split_whitespace()
+            .nth(5)
+            .ok_or_else(|| anyhow::anyhow!("Wrong unmatched monkey identifier"))?
+            .parse::<u32>()?;
+
+        Ok(Test {
+            divisor,
+            matched_receiver,
+            unmatched_receiver,
+        })
+    }
+}
+
 #[derive(Debug)]
 pub struct Monkey {
     pub items: Vec<u32>,
@@ -54,61 +82,46 @@ impl FromIterator<Monkey> for Monkeys {
     }
 }
 
+impl Monkey {
+    pub fn new(
+        (items, op, divisor, matched, unmatched): (&str, &str, &str, &str, &str),
+    ) -> Result<Self> {
+        let items = items
+            .split_whitespace()
+            .skip(2)
+            .map(|item| item.replace(',', "").parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+
+        let mut operation = op
+            .split_whitespace()
+            .skip(4)
+            .tuples()
+            .map(|(op, right)| Operation::new((op, right)))
+            .collect::<Vec<_>>();
+        assert_eq!(operation.len(), 1);
+        let operation = operation.pop().unwrap();
+
+        let test = Test::new(divisor, matched, unmatched)?;
+
+        Ok(Self {
+            items,
+            operation,
+            test,
+        })
+    }
+}
+
 impl Monkeys {
     pub fn new(file: &str) -> Result<Self> {
         let monkeys: Monkeys = read_to_string(file)?
             .lines()
             .tuples()
-            .map(|(_, items, op, test, matched, unmatched, _)| {
-                let items = items
-                    .split_whitespace()
-                    .skip(2)
-                    .map(|item| item.replace(',', "").parse::<u32>().unwrap())
-                    .collect::<Vec<_>>();
-
-                let mut operation = op
-                    .split_whitespace()
-                    .skip(4)
-                    .tuples()
-                    .map(|(op, right)| Operation::new((op, right)))
-                    .collect::<Vec<_>>();
-                assert_eq!(operation.len(), 1);
-                let operation = operation.pop().unwrap();
-
-                let divisor = test
-                    .split_whitespace()
-                    .nth(3)
-                    .unwrap()
-                    .parse::<u32>()
-                    .unwrap();
-
-                let matched_receiver = matched
-                    .split_whitespace()
-                    .nth(5)
-                    .unwrap()
-                    .parse::<u32>()
-                    .unwrap();
-
-                let unmatched_receiver = unmatched
-                    .split_whitespace()
-                    .nth(5)
-                    .unwrap()
-                    .parse::<u32>()
-                    .unwrap();
-
-                let test = Test {
-                    divisor,
-                    matched_receiver,
-                    unmatched_receiver,
-                };
-
-                Monkey {
-                    items,
-                    operation,
-                    test,
-                }
-            })
-            .collect();
+            .map(
+                |(_, items, op, test, matched, unmatched, _)| -> Result<Monkey> {
+                    Monkey::new((items, op, test, matched, unmatched))
+                },
+            )
+            .collect::<Result<Monkeys>>()?;
 
         Ok(monkeys)
     }
