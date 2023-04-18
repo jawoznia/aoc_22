828f405a866ce7da239a7d769bd7461d96b31911
Jan Woźniak <jnwznk@gmail.com>
2023-04-19 00:25:29 +0200

    Ongoing

--- day_13/src/lib.rs
+++ day_13/src/lib.rs
@@ -1,5 +1,66 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use anyhow::Result;
+use std::{cell::RefCell, fs::read_to_string, rc::Rc};
+
+#[derive(Debug)]
+pub enum Signal {
+    Integer(u32),
+    List(RefCell<Vec<Rc<Signal>>>),
+}
+
+#[derive(Debug)]
+pub struct PacketPair {
+    pub left: Vec<Signal>,
+    pub right: Vec<Signal>,
+}
+
+#[derive(Debug)]
+pub struct PacketPairs(Vec<PacketPair>);
+
+pub fn load(file: &str) -> Result<Rc<Signal>> {
+    // chars: '[' ']' ',' 'integer'
+    // '[' -> push new vec to stack
+    // ']' -> pop vec from stack somewhere
+    // integer -> add to vec on top of stack
+    // ',' -> ignore
+    // let mut stack = LinkedList::new();
+    let output = Rc::new(Signal::List(RefCell::new(vec![])));
+    let mut current_depth = 0;
+    let mut current_signal = output.clone();
+
+    read_to_string(file)?.lines().for_each(|line| {
+        // let mut signal = Signal::List(vec![]);
+        line.chars().for_each(|c| match c {
+            '[' => match current_signal.clone().as_ref() {
+                Signal::List(vec) => {
+                    vec.borrow_mut()
+                        .push(Rc::new(Signal::List(RefCell::new(vec![]))));
+                    current_signal = vec.borrow().last().unwrap().clone();
+                    current_depth += 1;
+                }
+                Signal::Integer(_) => panic!("Current signal is an integer"),
+            },
+            ']' => match current_signal.as_ref() {
+                Signal::List(_) => {
+                    current_signal = output.clone();
+                    current_depth -= 1;
+                    for _ in 0..current_depth {
+                        if let Signal::List(vec) = current_signal.clone().as_ref() {
+                            current_signal = vec.borrow().last().unwrap().clone();
+                        }
+                    }
+                }
+                Signal::Integer(_) => panic!("Current signal is an integer"),
+            },
+            ',' => (),
+            _ => match current_signal.as_ref() {
+                Signal::List(vec) => vec
+                    .borrow_mut()
+                    .push(Rc::new(Signal::Integer(c.to_digit(10).unwrap()))),
+                Signal::Integer(_) => panic!("Unexpected integer"),
+            },
+        })
+    });
+    Ok(output)
 }
 
 #[cfg(test)]
@@ -7,8 +68,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_load() {
+        let signal = load("example.txt").unwrap();
+        println!("{:#?}", signal);
     }
 }
